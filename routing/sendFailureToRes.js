41cf29eecd0b19dedf41373ed607f5a81f00a884
Zuip <zuiohi@gmail.com>
2018-11-05 05:56:21 +0200

    Added some error handling functionalities that are useful in all turtle apis

---
+++ routing/sendFailureToRes.js
@@ -0,0 +1,13 @@
+module.exports = function(res) {
+
+  return function(statusCode, message) {
+
+    res.status(
+      statusCode
+    ).json({
+      message
+    });
+
+    return Promise.reject();
+  };
+};
---
+++ routing/validateLanguage.js
@@ -0,0 +1,20 @@
+let getLanguage = require('../integrations/languages/getLanguage');
+
+module.exports = function(language, sendFailure) {
+
+  if (typeof language === 'undefined') {
+    return sendFailure(
+      404,
+      'Missing mandatory get parameter: language'
+    );
+  }
+
+  return getLanguage.withCode(language).then(
+    () => Promise.resolve()
+  ).catch(
+    () => sendFailure(
+      400,
+      'Invalid get parameter: language'
+    )
+  );
+};
